d3c0161e4be771f014172c94a8a57f2cdeea5cbf
Elvis Ciuffetelli <ciuffetellielvis@gmail.com>
2024-10-07 12:36:53 +0200

    redirect uri change

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ export default function Login() {
 		const { error } = await supabase.auth.signInWithOAuth({
 			provider: "google",
 			options: {
-				redirectTo: `${window.location.origin}/auth/callback`,
+				redirectTo: "https://blue-bear-gym.netlify.app/auth/callback",
 			},
 		});
 
